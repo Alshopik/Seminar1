7fd3ab9ce47790204a888541853f77813c57af20
aleksey <alshopik@yandex.ru>
2022-11-30 20:39:48 +0300

    Квадрат числа

---
+++ Ex001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("start"); // начало проекта
+
+int number = 15; // Определили целое число
+int result = number * number; // вычеслили квадрат числа
+Console.WriteLine(result); // Вывод результата
+
+Console.WriteLine("end"); // окончание проекта
